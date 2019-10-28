dd57a2ae19a64db26f9f0f3a78ec8dadfff28af0
Andrew <amayo0925@gmail.com>
2019-10-28 17:48:26 -0400

    finished simple weather app

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ window.addEventListener('load', ()=> {
     let temperatureDescription = document.querySelector('.temperature-description');
     let temperatureDegree = document.querySelector('.temperature-degree');
     let locationTimezone = document.querySelector('.location-timezone');
+    let temperatureSection = document.querySelector('.temperature');
+    const temperatureSpan = document.querySelector('.temperature span');
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => {
@@ -21,12 +23,22 @@ window.addEventListener('load', ()=> {
             .then(data => {
                 const {temperature, summary, icon} = data.currently;
                 //Set DOM ELements from the API
-                temperatureDegree.textContent = temperature;
+                temperatureDegree.textContent = Math.floor(temperature);
                 temperatureDescription.textContent = summary;
                 locationTimezone.textContent = data.timezone;
+                //Formula for Celsius 
+                let celsius = (temperature - 32) * (5 / 9);
                     setIcons(icon, document.querySelector('.icon'));
-
-                //
+                //Change temperature to Celsius/Farenheit
+                temperatureSection.addEventListener("click", () => {
+                    if(temperatureSpan.textContent === "F"){
+                        temperatureSpan.textContent = "C";
+                        temperatureDegree.textContent = Math.floor(celsius);
+                    }else{
+                        temperatureSpan.textContent = "F";
+                        temperatureDegree.textContent = Math.floor(temperature);
+                    }
+                })
             });
 
         });
